78efcee26ed5b000fba40ba7531972c4a2e56848
Kyryl313 <flash73627@gmail.com>
2025-12-09 22:10:16 +0200

    Add files via upload

---
+++ Lab8/products.py
@@ -0,0 +1,5 @@
+products = {
+    1: {"name": "Товар 1", "price": "250 грн", "photo": "https://via.placeholder.com/300x200.png"},
+    2: {"name": "Товар 2", "price": "300 грн", "photo": "https://via.placeholder.com/300x200.png"},
+    3: {"name": "Товар 3", "price": "200 грн", "photo": "https://via.placeholder.com/300x200.png"}
+}
